a53d4f9b40ce171a1bdc8066ae70d1cbaec80500
mura-work <murata.naoya.work@gmail.com>
2024-03-15 18:16:01 +0900

    [feat] chakraUIの導入

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {
+    mainColor: "#005E53",
+    headerColor: "#00B267",
+    primaryButtonColor: "#F1E500",
+    errorMessageColor: "#FF3F3F",
+  },
+});
+
+export default theme;
